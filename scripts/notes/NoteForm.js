135a3086c4d8413b2e27a6ade88b4499037ceb15
jamesnitz <jamesanitz2@gmail.com>
2020-07-27 10:18:29 -0500

    note post works.

--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -1,4 +1,5 @@
 import { useCriminals, getCriminals } from "../criminals/CriminalProvider.js"
+import { saveNote } from "./NoteProvider.js"
 
 const contentTarget = document.querySelector(".noteFormContainer")
 const eventHub = document.querySelector(".container")
@@ -6,7 +7,7 @@ const eventHub = document.querySelector(".container")
 const render = () => {
     getCriminals().then(() => {
         const criminalsCollection = useCriminals()
-        
+
         contentTarget.innerHTML = `
         <fieldset>
         <label class="label" for="note-text">Note:</>
@@ -21,11 +22,33 @@ const render = () => {
         })}
         </select>
         </fieldset>
-        
         <button id="saveNote">Save Note</button>
         `
     })
 }
+
+eventHub.addEventListener("click", clickEvent => {
+    if (clickEvent.target.id === "saveNote") {
+
+        const noteText = document.getElementById("note-text")
+        const noteCriminalId = document.getElementById("note-criminal")
+
+        // Make a new object representation of a note
+        const newNote = {
+            text: noteText.value,
+            criminalId: Number(noteCriminalId.value),
+            timestamp: new Date(Date.now()).toLocaleDateString()
+        }
+
+        // Change API state and application state
+        saveNote(newNote)
+        
+        //reset values of form
+        noteText.value = ""
+        noteCriminalId.value = 0
+
+    }
+})
 
 export const NoteForm = () => {
   render()
